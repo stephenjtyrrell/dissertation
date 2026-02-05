1cfd62be6ed9268ac3555f75c1ee5e53e8c06b91
stephenjtyrrell <stetyrrell132@gmail.com>
2026-02-05 22:46:26 +0000

    feat: Add AWS flow logs configuration with IAM roles and policies

--- infra/terraform/modules/aws/main.tf
+++ infra/terraform/modules/aws/main.tf
@@ -5,6 +5,62 @@ resource "aws_vpc" "this" {
   tags                 = merge(var.tags, { Name = "${var.name_prefix}-vpc" })
 }
 
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  name              = "/aws/vpc/${var.name_prefix}-flow-logs"
+  retention_in_days = 7
+  tags              = var.tags
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name = "${var.name_prefix}-vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  name = "${var.name_prefix}-vpc-flow-logs-policy"
+  role = aws_iam_role.flow_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_flow_log" "this" {
+  iam_role_arn    = aws_iam_role.flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.flow_logs.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.this.id
+
+  tags = merge(var.tags, { Name = "${var.name_prefix}-vpc-flow-log" })
+}
+
 resource "aws_subnet" "this" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.subnet_cidr_block

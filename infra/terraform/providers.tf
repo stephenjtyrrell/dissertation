c5061ddbaed27c5b0e9c1474d6164dc46cfb3a78
stephenjtyrrell <stetyrrell132@gmail.com>
2026-02-05 22:25:13 +0000

    Restore root-level Terraform providers and prevent cross-cloud credential loading (#10)
    * Avoid cross-cloud credential loading in Terraform
    
    * Avoid dummy credentials for inactive providers

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,9 +1,21 @@
+locals {
+  aws_enabled   = var.cloud == "aws"
+  azure_enabled = var.cloud == "azure"
+  gcp_enabled   = var.cloud == "gcp"
+}
+
 provider "aws" {
   region = var.aws_region
+
+  skip_credentials_validation = !local.aws_enabled
+  skip_requesting_account_id  = !local.aws_enabled
 }
 
 provider "azurerm" {
   features {}
+
+  use_cli                    = local.azure_enabled
+  skip_provider_registration = !local.azure_enabled
 }
 
 provider "google" {
